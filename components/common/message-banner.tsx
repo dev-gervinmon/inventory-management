5f0b47a478f31c275b45c61389b202555f4d1c90
dev-gervinmon <dev.montibon.geraldvincent54@gmail.com>
2025-12-19 11:10:22 +0800

    feat: add MessageBanner component for displaying success and error messages

---
+++ components/common/message-banner.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Message } from "@/lib/hooks/useMessage";
+
+interface MessageBannerProps {
+  message: Message;
+}
+
+export default function MessageBanner({ message }: MessageBannerProps) {
+  if (!message.text) return null;
+
+  const isError = message.type === "error";
+  const isSuccess = message.type === "success";
+
+  return (
+    <div
+      className={`text-sm p-3 rounded-lg border transition-all duration-300 ${
+        isError
+          ? "bg-red-50 text-red-700 border-red-200"
+          : isSuccess
+          ? "bg-green-50 text-green-700 border-green-200"
+          : ""
+      }`}
+    >
+      {message.text}
+    </div>
+  );
+}
