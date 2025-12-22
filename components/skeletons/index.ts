d25fe0efef59ea50d39655624c56a665b1f1783c
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2025-12-22 10:23:48 +0800

    feat: add index file for exporting all skeleton components for easy access

---
+++ components/skeletons/index.ts
@@ -0,0 +1,32 @@
+/**
+ * Skeleton Components Index
+ * Export all skeleton components for easy access across the app
+ */
+
+// Base skeleton
+export { Skeleton } from "./skeleton";
+
+// Dashboard skeletons
+export {
+  DashboardHeaderSkeleton,
+  MetricCardSkeleton,
+  DashboardMetricsSkeleton,
+  ChartSkeleton,
+  ChartRowSkeleton,
+  AlertActivityItemSkeleton,
+  AlertsActivityTabsSkeleton,
+  DashboardSkeleton,
+} from "./dashboard-skeleton";
+
+// Generic reusable skeletons
+export {
+  CardSkeleton,
+  TableRowSkeleton,
+  GridSkeleton,
+  FormSkeleton,
+  ListSkeleton,
+  HeaderSkeleton,
+} from "./generic-skeletons";
+
+// Loading state component
+export { LoadingState } from "./loading-state";
