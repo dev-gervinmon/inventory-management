50707c780b627b71cfa33e8a0dc889c59dc3cef1
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2025-12-29 10:59:36 +0800

    fix: remove unnecessary padding for sticky header in ProductEditClient

--- components/clients/product-edit-client.tsx
+++ components/clients/product-edit-client.tsx
@@ -249,8 +249,6 @@ export default function ProductEditClient({
       />
 
       {/* Add padding for sticky header - responsive */}
-      <div className="pt-6 sm:pt-8 md:pt-20" />
-
       <div>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 sm:gap-6 md:gap-8">
           {/* Left Column: Form */}
