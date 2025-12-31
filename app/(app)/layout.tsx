24485571f106416c89de058cae81c738fd5ff246
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2025-12-31 19:21:04 +0800

    feat: refactor AppLayout to use a div wrapper for improved structure and styling

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,11 +1,19 @@
+import "@/styles/theme.css";
 import AppShellClient from "@/components/clients/app-shell-client";
 import TopNavBarServer from "@/components/layout/top-navbar-server";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div
+      className="
+        min-h-screen
+        bg-[rgb(var(--surface-app))]
+        text-[rgb(var(--text-primary))]
+        antialiased
+      "
+    >
       <TopNavBarServer />
       <AppShellClient>{children}</AppShellClient>
-    </>
+    </div>
   );
 }
